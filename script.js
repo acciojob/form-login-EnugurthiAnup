315815368d4484edee5f9a15a5d610ae30f66b54
EnugurthiAnup <eanup.736@gmail.com>
2025-11-17 22:36:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+let first = document.querySelector('[name="fname"]')
+let last = document.querySelector('[name="lname"]')
+
+
 function getFormvalue() {
     //Write your code here
+	alert(first.value +"" + last.value)
 
 }
+
